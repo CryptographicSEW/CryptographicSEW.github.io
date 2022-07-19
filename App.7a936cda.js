0ce7f5a79c05890363c5530dcbf9f16f87c91272
Ilyar <761285+ilyar@users.noreply.github.com>
2022-07-19 05:28:37 +0200

    Updates

--- App.7a936cda.js
+++ App.7a936cda.js
@@ -47295,20 +47295,177 @@ exports.userRender = userRender;
 },{"../browser":"browser.ts","./util":"ui/util.ts"}],"ui/betList.ts":[function(require,module,exports) {
 "use strict";
 
+var __awaiter = this && this.__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+var __generator = this && this.__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function sent() {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) {
+      try {
+        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [op[0] & 2, t.value];
+
+        switch (op[0]) {
+          case 0:
+          case 1:
+            t = op;
+            break;
+
+          case 4:
+            _.label++;
+            return {
+              value: op[1],
+              done: false
+            };
+
+          case 5:
+            _.label++;
+            y = op[1];
+            op = [0];
+            continue;
+
+          case 7:
+            op = _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+
+          default:
+            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+              _ = 0;
+              continue;
+            }
+
+            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+              _.label = op[1];
+              break;
+            }
+
+            if (op[0] === 6 && _.label < t[1]) {
+              _.label = t[1];
+              t = op;
+              break;
+            }
+
+            if (t && _.label < t[2]) {
+              _.label = t[2];
+
+              _.ops.push(op);
+
+              break;
+            }
+
+            if (t[2]) _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+        }
+
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [6, e];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
+
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.betListRender = exports.redeemRender = exports.beatRender = void 0;
+exports.betListRender = exports.redeemRender = exports.actionRender = exports.beatRender = void 0;
 
 var browser_1 = require("../browser");
 
 var util_1 = require("./util");
 
 function beatRender(value) {
-  return "<div class=\"btn-group\" role=\"group\">\n    <button\n        data-behavior=\"action\"\n        data-name=\"beat\"\n        data-param='{\"id\":\"".concat(value.id, "\", \"chose\": \"S\"}'\n        type=\"button\"\n        class=\"btn btn-secondary\">S</button>\n    <button\n        data-behavior=\"action\"\n        data-name=\"beat\"\n        data-param='{\"id\":\"").concat(value.id, "\", \"chose\": \"E\"}' \n        type=\"button\" class=\"btn btn-secondary\">E</button>\n    <button\n        data-behavior=\"action\"\n        data-name=\"beat\"\n        data-param='{\"id\":\"").concat(value.id, "\", \"chose\": \"W\"}'\n        type=\"button\" class=\"btn btn-secondary\">W</button>\n</div>");
+  return "<div class=\"btn-group\" role=\"group\">\n    <button\n        data-behavior=\"action\"\n        data-name=\"beat\"\n        data-param='{\"id\":\"".concat(value.id, "\", \"chose\": \"S\"}'\n        type=\"button\"\n        class=\"btn btn-secondary\">S</button>\n    <button\n        data-behavior=\"action\"\n        data-name=\"beat\"\n        data-param='{\"id\":\"").concat(value.id, "\", \"chose\": \"E\"}' \n        type=\"button\" class=\"btn btn-secondary\">E</button>\n    <button\n        data-behavior=\"action\"\n        data-name=\"beat\"\n        data-param='{\"id\":\"").concat(value.id, "\", \"chose\": \"W\"}'\n        type=\"button\" class=\"btn btn-secondary\">W</button>\n    <button\n        data-behavior=\"action\"\n        data-name=\"beat\"\n        data-param='{\"id\":\"").concat(value.id, "\", \"chose\": \"R\"}'\n        type=\"button\" class=\"btn btn-secondary\">R</button>\n</div>");
 }
 
 exports.beatRender = beatRender;
+
+function actionRender(currentUser, value) {
+  var isOwner = value.user.toString() == currentUser;
+
+  if (value.beat && isOwner) {
+    return redeemRender(value);
+  } else if (!value.beat) {
+    return beatRender(value);
+  }
+
+  return '<div class="alert alert-warning" role="alert">waiting</div>';
+}
+
+exports.actionRender = actionRender;
 
 function redeemRender(value) {
   return "<div class=\"btn-group\" role=\"group\">\n    <button \n    data-behavior=\"action\"\n    data-name=\"redeem\"\n    data-param='{\"id\":\"".concat(value.id, "\", \"hash\": \"").concat(value.hash, "\"}'\n    type=\"button\" class=\"btn btn-secondary\">redeem</button>\n</div>");
@@ -47316,14 +47473,21 @@ function redeemRender(value) {
 
 exports.redeemRender = redeemRender;
 
-function betListRender(list) {
-  console.log(list);
-  var out = '';
-  list.forEach(function (value, key) {
-    out += "<div class=\"card\" style=\"width: 18rem;\">\n                ".concat((0, util_1.avatarsRender)(value.hash, 'identicon'), "\n                <div class=\"card-body\">\n                    <h1 class=\"card-title\">").concat(value.amount.toFixed(0), "\u0116</h1>\n                    <div class=\"card\">\n                        <div class=\"d-flex flex-row\">\n                            <div class=\"p-2\">").concat((0, util_1.avatarsRender)(value.user.toString()), "</div>\n                            <div class=\"p-2\">").concat(value.beat ? (0, util_1.avatarsRender)(value.beat.user.toString()) : '<h1>?</h1>', "</div>\n                        </div>\n                    </div>\n                </div>\n                <div class=\"card-body\">").concat(value.beat ? redeemRender(value) : beatRender(value), "</div>\n            </div>");
-  });
-  (0, browser_1.behavior)('betList', function (elem) {
-    return elem.innerHTML = out;
+function betListRender(currentUser, list) {
+  return __awaiter(this, void 0, void 0, function () {
+    var out;
+    return __generator(this, function (_a) {
+      out = '';
+      list.forEach(function (value, key) {
+        out += "<div class=\"card\" style=\"width: 18rem;\">\n                ".concat((0, util_1.avatarsRender)(value.hash, 'identicon'), "\n                <div class=\"card-body\">\n                    <h1 class=\"card-title\">").concat(value.amount.toFixed(0), "\u0116</h1>\n                    <div class=\"card\">\n                        <div class=\"d-flex flex-row\">\n                            <div class=\"p-2\">").concat((0, util_1.avatarsRender)(value.user.toString()), "</div>\n                            <div class=\"p-2\">").concat(value.beat ? (0, util_1.avatarsRender)(value.beat.user.toString()) : '<h1>?</h1>', "</div>\n                        </div>\n                    </div>\n                    ").concat(actionRender(currentUser, value), "\n                </div>\n            </div>");
+      });
+      (0, browser_1.behavior)('betList', function (elem) {
+        return elem.innerHTML = out;
+      });
+      return [2
+      /*return*/
+      ];
+    });
   });
 }
 
@@ -47520,6 +47684,7 @@ var __importDefault = this && this.__importDefault || function (mod) {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.currentUser = void 0;
 
 var everscale_inpage_provider_1 = require("everscale-inpage-provider");
 
@@ -47544,6 +47709,35 @@ var provider = {
   everscale: null
 };
 var GameContract;
+var nameOptionList = ['S', 'E', 'W'];
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + max);
+}
+
+function currentUser() {
+  return __awaiter(this, void 0, Promise, function () {
+    return __generator(this, function (_a) {
+      switch (_a.label) {
+        case 0:
+          return [4
+          /*yield*/
+          , (0, blockchain_1.selectedConnection)('everscale', provider)];
+
+        case 1:
+          return [2
+          /*return*/
+          , _a.sent().address];
+      }
+    });
+  });
+}
+
+exports.currentUser = currentUser;
+
+function nameOptionListRandom() {
+  return nameOptionList[random(0, 2)];
+}
 
 function _bet(nameOption) {
   return __awaiter(this, void 0, Promise, function () {
@@ -47554,6 +47748,10 @@ function _bet(nameOption) {
     return __generator(this, function (_d) {
       switch (_d.label) {
         case 0:
+          if (nameOption == 'R') {
+            nameOption = nameOptionListRandom();
+          }
+
           return [4
           /*yield*/
           , gameContract()];
@@ -47576,9 +47774,9 @@ function _bet(nameOption) {
         case 3:
           return [4
           /*yield*/
-          , _b.apply(_a, [(_c.betHash = _d.sent(), _c.amount = '14000000000', _c)]).send({
+          , _b.apply(_a, [(_c.betHash = _d.sent(), _c.amount = '100000000000', _c)]).send({
             from: selectedAddress,
-            amount: '15000000000',
+            amount: '101000000000',
             bounce: true
           })];
 
@@ -47599,6 +47797,10 @@ function _beat(betId, chose) {
     return __generator(this, function (_a) {
       switch (_a.label) {
         case 0:
+          if (chose == 'R') {
+            chose = nameOptionListRandom();
+          }
+
           return [4
           /*yield*/
           , gameContract()];
@@ -47743,7 +47945,7 @@ function listBet() {
         case 2:
           out = _a.sent();
           list = new Map();
-          out.listBet.forEach(function (it) {
+          out.listBet.slice(0, 10).forEach(function (it) {
             list.set(it[0], {
               id: it[0],
               amount: new bignumber_js_1.BigNumber(it[1].amount).shiftedBy(-9).toNumber(),
@@ -48167,23 +48369,29 @@ function mainFlow() {
 
 function refresh() {
   return __awaiter(this, void 0, void 0, function () {
-    var _a;
+    var _a, _b;
 
-    return __generator(this, function (_b) {
-      switch (_b.label) {
+    return __generator(this, function (_c) {
+      switch (_c.label) {
         case 0:
           _a = ui_1.betListRender;
           return [4
           /*yield*/
-          , listBet()];
+          , currentUser()];
 
         case 1:
+          _b = [_c.sent()];
           return [4
           /*yield*/
-          , _a.apply(void 0, [_b.sent()])];
+          , listBet()];
 
         case 2:
-          _b.sent();
+          return [4
+          /*yield*/
+          , _a.apply(void 0, _b.concat([_c.sent()]))];
+
+        case 3:
+          _c.sent();
 
           (0, browser_1.action)(actionList);
           return [2
@@ -48258,7 +48466,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34941" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34509" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
